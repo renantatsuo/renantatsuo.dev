16a0e766749c33d40f9703c5e3a3f6bc6a1c99e7
renantatsuo <tatsuorenan@gmail.com>
2020-07-02 22:38:34 -0300

    create tests for Posts

---
+++ tests/lib/Posts.test.ts
@@ -0,0 +1,52 @@
+import { mock } from "jest-mock-extended";
+import { container } from "tsyringe";
+import { getPost, getPosts } from "../../lib/Posts";
+import Post from "../../models/Post";
+import { FileSystem } from "./../../lib/filesystem/FileSystem";
+
+const fsMock = mock<FileSystem>();
+
+container.register("FileSystem", {
+  useValue: fsMock,
+});
+
+describe("Test getPosts()", () => {
+  it("Should return no posts", async () => {
+    fsMock.listFiles.mockResolvedValueOnce([]);
+
+    expect(await getPosts()).toStrictEqual([]);
+  });
+
+  it("Should return [2] posts", async () => {
+    const FILENAME = "filename";
+    fsMock.listFiles.mockResolvedValueOnce([FILENAME, FILENAME]);
+    fsMock.readFile.mockResolvedValue(Buffer.of());
+
+    expect(await getPosts()).toHaveLength(2);
+  });
+});
+
+describe("Test getPost()", () => {
+  it("Should return specific post", async () => {
+    const FILENAME = "filename";
+    const postString = [
+      "---",
+      "title: test post",
+      "slug: test-post",
+      `createdAt: "1970-01-01"`,
+      "---",
+      "this is a test post",
+    ].join("\n");
+    fsMock.readFile.mockResolvedValueOnce(Buffer.from(postString));
+
+    const expectedPost: Post = {
+      title: "test post",
+      slug: "test-post",
+      createdAt: "1970-01-01",
+      content: "this is a test post",
+      excerpt: "this is a test post...",
+    };
+
+    expect(await getPost(FILENAME)).toStrictEqual(expectedPost);
+  });
+});
