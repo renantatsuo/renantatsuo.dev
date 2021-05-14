1d5df4a76a6f5e3b42355f765d66306965fe45a3
renantatsuo <tatsuorenan@gmail.com>
2021-05-14 17:41:15 -0300

    Upgrade ReactMarkdown

--- components/CodeHighlighter/index.tsx
+++ components/CodeHighlighter/index.tsx
@@ -2,17 +2,23 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import theme from "../../themes/CodeHighlighterTheme";
 
 type CodeHighlighterProps = {
-  value: string;
-  language: string;
+  value: string | React.ReactNode[];
+  inline: boolean;
+  className: string;
 };
 
 export default function CodeHighlighter({
-  language,
   value,
+  inline,
+  className,
 }: CodeHighlighterProps) {
+  if (inline) {
+    return <code className={className}>{value}</code>;
+  }
+  const language = /language-(\w+)/.exec(className ?? "");
   return (
-    <SyntaxHighlighter language={language} style={theme}>
-      {value}
+    <SyntaxHighlighter language={language[1]} style={theme}>
+      {String(value).replace(/\n$/, "")}
     </SyntaxHighlighter>
   );
 }
--- components/PostContent/index.tsx
+++ components/PostContent/index.tsx
@@ -1,8 +1,9 @@
-import CodeHighlighter from "@components/CodeHighlighter";
+import CH from "@components/CodeHighlighter";
 import FormattedDate from "@lib/date/FormattedDate";
 import { DATE_FORMATTER_OPTIONS } from "@lib/static";
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
 import { PostContainer, PostDate, PostHeader, PostTitle } from "./styles";
 
 type PostProps = {
@@ -20,9 +21,33 @@ export default function PostContent({ post }: PostProps) {
         <PostDate>published on {formattedCreatedAt.toString()}</PostDate>
       </PostHeader>
       <ReactMarkdown
-        source={post.content}
-        renderers={{ code: CodeHighlighter }}
+        children={post.content}
+        remarkPlugins={[gfm]}
+        components={{
+          pre: FixedPreComponent,
+          code: CodeHighlighter,
+        }}
       />
     </PostContainer>
+  );
+}
+
+function FixedPreComponent(component) {
+  const isCode = component.node.children[0].tagName == "code";
+
+  if (isCode) {
+    return component.children;
+  }
+
+  return <pre>{component.children}</pre>;
+}
+
+function CodeHighlighter({ node, ...props }) {
+  return (
+    <CH
+      className={props.className}
+      inline={props.inline}
+      value={props.children}
+    />
   );
 }
