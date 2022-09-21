6c6857ef89e58812e7a8e716686e3f8b7f21e341
renantatsuo <tatsuorenan@gmail.com>
2022-09-20 21:23:00 -0300

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
-module.exports = {
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+module.exports = createJestConfig({
   setupFiles: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
     "~/(.*)": "<rootDir>/$1",
   },
-};
+});
