3957fb4f307fc9c33040f17df5ef3c3c51ff5253
renantatsuo <tatsuorenan@gmail.com>
2026-01-19 16:07:08 +0000

    fix nitro build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
+import { defineConfig, Plugin } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
@@ -9,6 +11,8 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
+    copyResources(),
+    devtools(),
     tailwindcss(),
     tsconfigPaths(),
     tanstackStart({
@@ -16,10 +20,45 @@ export default defineConfig({
       router: {
         routesDirectory: "pages",
       },
+      prerender: {
+        enabled: true,
+        crawlLinks: true,
+        autoStaticPathsDiscovery: true,
+      },
       sitemap: {
-        host: "https://renan.dev",
+        enabled: true,
+        host: process.env.VERCEL_URL
+          ? `https://${process.env.VERCEL_URL}`
+          : "http://localhost:3000",
       },
     }),
+    nitro(),
     viteReact(),
   ],
+  nitro: {
+    publicAssets: [
+      {
+        dir: "dist/client/__tsr",
+        baseURL: "/__tsr",
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+      },
+    ],
+  },
 });
+
+async function copyResources(): Promise<Plugin> {
+  // this is a workaround to copy the resources to the build directory
+  // so the preview server can serve the resources and the prerender can work
+  // properly.
+  const fs = await import("fs");
+  const path = await import("path");
+  const posts = path.resolve(__dirname, "resources");
+  const dist = path.resolve(__dirname, ".output/resources");
+  fs.cpSync(posts, dist, { recursive: true });
+  return {
+    name: "copy-resources",
+    writeBundle: () => {
+      return Promise.resolve();
+    },
+  };
+}
